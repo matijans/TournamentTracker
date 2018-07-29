dfacc4a1f986d384ca021af66cc12353b14743da
matijans <stjepan.matijanic@gmail.com>
2018-07-29 23:57:10 +0200

    Create team form part 3 completed

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -12,6 +12,7 @@ namespace TrackerLibrary.DataAccess
     {
         private const string PrizesFile = "PrizeModels.csv";
         private const string PeopleFile = "PersonModels.csv";
+        private const string TeamFile = "TeamModels.csv";
 
         public PersonModel CreatePerson(PersonModel model)
         {
@@ -50,6 +51,26 @@ namespace TrackerLibrary.DataAccess
             prizes.Add(model);
 
             prizes.SaveToPrizeFile(PrizesFile);
+
+            return model;
+        }
+
+        public TeamModel CreateTeam(TeamModel model)
+        {
+            List<TeamModel> teams = TeamFile.FullFilePath().LoadFile().ConvertToTeamModels(PeopleFile);
+
+            int currentID = 1;
+
+            if (teams.Count > 0)
+            {
+                currentID = teams.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentID;
+
+            teams.Add(model);
+
+            teams.SaveToTeamFile(TeamFile);
 
             return model;
         }
